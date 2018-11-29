817565fe2857782f2d10b39ad4bc8a59a7c147b5
Truong <tgv99hy@gmail.com>
2018-11-29 09:20:41 +0700

    Đéo

--- CanteenManagementApp/src/Canteen/Main.java
+++ CanteenManagementApp/src/Canteen/Main.java
@@ -12,6 +12,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
 		System.out.print("trung dep trai bu cu");
-    }
+		System.out.print("Đéo");    }
     
 }
